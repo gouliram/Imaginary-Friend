640a51b75d74fc305ec0c8170108fc320ffbc5b0
Gouliram <gouliram@gmail.com>
2021-05-23 14:26:07 -0400

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.Ends = New System.Windows.Forms.Button()
+        Me.Followers = New System.Windows.Forms.Button()
+        Me.Subscriptions = New System.Windows.Forms.Button()
+        Me.Donations = New System.Windows.Forms.Button()
+        Me.Bits = New System.Windows.Forms.Button()
+        Me.Hugs = New System.Windows.Forms.Button()
+        Me.Treats = New System.Windows.Forms.Button()
+        Me.Level = New System.Windows.Forms.Label()
+        Me.Progress = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Start
+        '
+        Me.Start.AllowDrop = True
+        Me.Start.Location = New System.Drawing.Point(260, 278)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(112, 31)
+        Me.Start.TabIndex = 0
+        Me.Start.Text = "Start Stream"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'Ends
+        '
+        Me.Ends.Location = New System.Drawing.Point(407, 278)
+        Me.Ends.Name = "Ends"
+        Me.Ends.Size = New System.Drawing.Size(112, 31)
+        Me.Ends.TabIndex = 1
+        Me.Ends.Text = "End Stream"
+        Me.Ends.UseVisualStyleBackColor = True
+        '
+        'Followers
+        '
+        Me.Followers.Location = New System.Drawing.Point(260, 315)
+        Me.Followers.Name = "Followers"
+        Me.Followers.Size = New System.Drawing.Size(112, 31)
+        Me.Followers.TabIndex = 2
+        Me.Followers.Text = "Followers"
+        Me.Followers.UseVisualStyleBackColor = True
+        '
+        'Subscriptions
+        '
+        Me.Subscriptions.Location = New System.Drawing.Point(407, 315)
+        Me.Subscriptions.Name = "Subscriptions"
+        Me.Subscriptions.Size = New System.Drawing.Size(112, 31)
+        Me.Subscriptions.TabIndex = 3
+        Me.Subscriptions.Text = "Subscriptions"
+        Me.Subscriptions.UseVisualStyleBackColor = True
+        '
+        'Donations
+        '
+        Me.Donations.Location = New System.Drawing.Point(260, 352)
+        Me.Donations.Name = "Donations"
+        Me.Donations.Size = New System.Drawing.Size(112, 31)
+        Me.Donations.TabIndex = 4
+        Me.Donations.Text = "Donations"
+        Me.Donations.UseVisualStyleBackColor = True
+        '
+        'Bits
+        '
+        Me.Bits.Location = New System.Drawing.Point(407, 352)
+        Me.Bits.Name = "Bits"
+        Me.Bits.Size = New System.Drawing.Size(112, 31)
+        Me.Bits.TabIndex = 5
+        Me.Bits.Text = "Bits"
+        Me.Bits.UseVisualStyleBackColor = True
+        '
+        'Hugs
+        '
+        Me.Hugs.Location = New System.Drawing.Point(260, 389)
+        Me.Hugs.Name = "Hugs"
+        Me.Hugs.Size = New System.Drawing.Size(112, 31)
+        Me.Hugs.TabIndex = 6
+        Me.Hugs.Text = "Hugs"
+        Me.Hugs.UseVisualStyleBackColor = True
+        '
+        'Treats
+        '
+        Me.Treats.Location = New System.Drawing.Point(407, 389)
+        Me.Treats.Name = "Treats"
+        Me.Treats.Size = New System.Drawing.Size(112, 31)
+        Me.Treats.TabIndex = 7
+        Me.Treats.Text = "Treats"
+        Me.Treats.UseVisualStyleBackColor = True
+        '
+        'Level
+        '
+        Me.Level.AutoSize = True
+        Me.Level.Location = New System.Drawing.Point(371, 222)
+        Me.Level.Name = "Level"
+        Me.Level.Size = New System.Drawing.Size(34, 15)
+        Me.Level.TabIndex = 8
+        Me.Level.Text = "Level"
+        '
+        'Progress
+        '
+        Me.Progress.AutoSize = True
+        Me.Progress.Location = New System.Drawing.Point(550, 222)
+        Me.Progress.Name = "Progress"
+        Me.Progress.Size = New System.Drawing.Size(26, 15)
+        Me.Progress.TabIndex = 9
+        Me.Progress.Text = "0 %"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(211, 240)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(365, 23)
+        Me.ProgressBar1.TabIndex = 10
+        '
+        'Timer
+        '
+        Me.Timer.Enabled = True
+        Me.Timer.Interval = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Progress)
+        Me.Controls.Add(Me.Level)
+        Me.Controls.Add(Me.Treats)
+        Me.Controls.Add(Me.Hugs)
+        Me.Controls.Add(Me.Bits)
+        Me.Controls.Add(Me.Donations)
+        Me.Controls.Add(Me.Subscriptions)
+        Me.Controls.Add(Me.Followers)
+        Me.Controls.Add(Me.Ends)
+        Me.Controls.Add(Me.Start)
+        Me.Name = "Form1"
+        Me.Text = "Ami imaginaire"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Start As Button
+    Friend WithEvents Ends As Button
+    Friend WithEvents Followers As Button
+    Friend WithEvents Subscriptions As Button
+    Friend WithEvents Donations As Button
+    Friend WithEvents Bits As Button
+    Friend WithEvents Hugs As Button
+    Friend WithEvents Treats As Button
+    Friend WithEvents Level As Label
+    Friend WithEvents Progress As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    REM: Marilou Gagnon (Gouliram)
+    REM: Beggining of the process : 23-05-2021
+    REM: This needs to be an easy fix. So, the problem is the Main Sub doesnt allowed me to make changes. It opens and closes without being able to do something onto the app. 
+    REM: My twitch account : www.twitch.tv/gouliram
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+        ' With this button, i want it to be able to put this timer on, when im going on live. or press it when im on live on twitch. 
+        ' i want it to go by second on second and put some xp onto the xp bar that we're going to see in a little. 
+        Timer.Start()
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        'For every event going on my streams, this xp bar will be increasing and it will make up to some levels. for every levels, i want it to be able to save the progress between each sessions. 
+        ProgressBar1.Increment(1)
+        If ProgressBar1.Value = 100 Then
+            Level.Text = "Level up!"
+            'leveling up : i want it to be able to go on levels up to 1000. increasing the xp by some little modifiers. 
+        End If
+        Progress.Text = ProgressBar1.Value & (" %")
+    End Sub
+
+    Private Sub Ends_Click(sender As Object, e As EventArgs) Handles Ends.Click
+        Timer.Stop()
+        ' i want it to be able to keeps the xp that has been going on since the beginning of this project. When i do have to close my streaming session. 
+    End Sub
+
+    Private Sub Followers_Click(sender As Object, e As EventArgs) Handles Followers.Click
+        'For every followers, i want it to be able to add some xp to the bar. still less than a subscriptions or bits and donations. 
+    End Sub
+
+    Private Sub Subscriptions_Click(sender As Object, e As EventArgs) Handles Subscriptions.Click
+        'For every subscriptions, i want it to be able to add some xp to the bar. still less than a donation or bits
+    End Sub
+    Private Sub Bits_Click(sender As Object, e As EventArgs) Handles Bits.Click
+        'For every bits, i want it to be able to add some xp to the bar. still less than a donation. 
+        'If possible, can we have a textbox for putting the amount of bits donated?
+    End Sub
+    Private Sub Donations_Click(sender As Object, e As EventArgs) Handles Donations.Click
+        'For every donations, i want it to be able to add some xp to the bar.  
+        'If possible, can we have a textbox for putting the amount of money donated?
+    End Sub
+
+    Private Sub Hugs_Click(sender As Object, e As EventArgs) Handles Hugs.Click
+        'this is a bit more complicated im my honest opinion. this action is actually going on by the channels points. and for every 'hugs' that my viewers will be giving, it will affect the xp bar, still less than a subscription xp. 
+    End Sub
+
+    Private Sub Treats_Click(sender As Object, e As EventArgs) Handles Treats.Click
+        'this is a bit more complicated im my honest opinion. this action is actually going on by the channels points. and for every 'treats' that my viewers will be giving, it will affect the xp bar, still less than a bits xp. 
+    End Sub
+End Class
+Public Module mainModule
+    Sub Main()
+    End Sub
+End Module
